bbc0ddc5d582d8926a2e5104fe046a939a88ac1e
DarshKukreja <62602974+darsh32774@users.noreply.github.com>
2024-09-05 16:01:29 +0530

    Add files via upload

---
+++ Java Programs/Division.java
@@ -0,0 +1,9 @@
+package Calculator1;
+
+public class Division{
+	public int div(int num1, int num2){
+			return num1 / num2;
+	}
+}
+	
+
---
+++ Java Programs/Multiplication.java
@@ -0,0 +1,8 @@
+package Calculator1;
+
+public class Multiplication{
+	public int mul(int num1, int num2){
+			return num1 * num2;
+	}
+}
+	
