d6a9e4f208372b84034a75d5faa029a2a305c8f4
DarshKukreja <62602974+darsh32774@users.noreply.github.com>
2024-09-20 03:15:59 +0530

    Update Queue.C

--- C Programs/Queue.C
+++ C Programs/Queue.C
@@ -1,160 +1,170 @@
 #include<stdio.h>
+
+void EnQ();
+void DeQ();
 int isFull();
 int isEmpty();
-void Insert();
-void Delete();
-void pop(int);
 void Display();
 void Front();
-int arr[50] = {0},n;
-int top = -1;
+
+int Arr[10] = {0};
+int front = 0, rear = -1;
+
 int main()
 {
-	int choice;
-	
-	printf("Enter size of queue: ");
-	scanf("%d",&n);
-	
-	while(choice!=5)
-	{
-		printf("\n1. Insert\n2. Delete\n3. Display\n4. Front\n5. Exit\n");
-		scanf("%d",&choice);
-		switch(choice)
-		{
-			case 1:
-			{ 
-				Insert();
-				break;
-			}
-			case 2:
-			{
-				Delete();
-				break;
-			}
-			case 3:
-			{
-				Display();
-				break;
-			}
-			case 4:
-			{
-				Front();
-				break;
-			}
-			case 5:
-				break;
-			default: 
-				printf("Invalid input.");
-		}
-	}
-	
-	return 0;
+     int choice;
+	 //clrscr();
+
+	 while(choice!=5)
+	 {
+		  printf("1. Enqueue\n2. Dequeue\n3. Display\n4. Front\n5. Exit");
+		  scanf("%d",&choice);
+
+		  switch(choice)
+		  {
+			   case 1:
+			   {
+					EnQ();
+					break;
+			   }
+			   case 2:
+			   {
+					DeQ();
+					break;
+			   }
+			   case 3:
+			   {
+					Display();
+					break;
+			   }
+			   case 4:
+			   {
+					Front();
+					break;
+			   }
+			   case 5:
+					break;
+			   default:
+			   {
+					printf("Invalid input.");
+					break;
+			   }
+		  }
+	 }
+	 //getch();
+	 return 0;
 }
 
 int isFull()
 {
-	if(top == n-1)
-		return 1;
-	else
-		return 0;
+	 if(rear>=9)
+		  return 1;
+	 else
+          return 0;
 }
 
 int isEmpty()
 {
-	if(top == -1)
-		return 1;
-	else
-		return 0;
+     if(rear<0)
+          return 1;
+     else
+          return 0;
 }
 
-void Insert()
+void EnQ()
 {
-	int num, i;
-	
-	if(isFull())
-	{
-		printf("Queue is full.");
-		return;
-	}
-	
-	here:
-	printf("Enter number of elements to be inserted: ");
-	scanf("%d",&num);
-	
-	if(num>=n-top)
-	{
-		printf("Queue cannot accept %d elements.\n\n",num);
-		goto here;
-	}
-	else
-	{
-		for(i=0;i<num;i++)
-		{
-			printf("element %d: ",i+1);
-			scanf("%d",&arr[++top]);
-		}
-	}
-	
-	Display();
+     int el;
+     int n,i;
+
+     if(isFull())
+     {
+          printf("\nQueue is full.\n\n");
+          return;
+     }
+     else
+     {
+          here:
+          printf("\nEnter number of elements to enqueue: ");
+          scanf("%d",&n);
+          
+          if(n>(10-(rear+1)))
+          {
+              printf("Max size exceeded.\n");
+              goto here;
+          }
+          
+          for(i=1;i<=n;i++)
+          {
+               printf("Enter element %d: ",i);
+               scanf("%d",&el);
+               Arr[++rear] = el;
+          }
+
+          Display();    
+     }
 }
 
-void Delete()
+void DeQ()
 {
-	int num, i;
-	
-	if(isEmpty())
-	{
-		printf("Queue is empty.");
-		return;
-	}
-	
-	there:
-	printf("\nEnter number of elements to be deleted: ");
-	scanf("%d",&num);
-	
-	if(num>top+1)
-	{
-		printf("Queue does not have %d elements to delete.",num);
-		goto there;
-	}
-	else
-	{	
-		for(i=0;i<num;i++)
-		{
-			pop(num);
-		}
-	}
-	Display();
+     int n,i,j;
+
+     if(isEmpty())
+     {
+          printf("\nQueue is empty.\n\n");
+          return;
+     }
+     else
+     {
+          there:
+          printf("\nEnter number of elements to dequeue: ");
+          scanf("%d",&n);
+          
+          if(n>rear+1)
+          {
+              printf("Queue does not have %d elements.\n",n);
+              goto there;
+          }
+          
+          for(i=1;i<=n;i++)
+          {
+               for(j=0;j<=rear;j++)
+               {
+                  Arr[j] = Arr[j+1];
+               }
+          }
+          
+          rear-=n;
+
+          Display();
+     }
 }
 
 void Display()
 {
-	int i;
-	printf("\nQUEUE: ");
-	for(i=0;i<=top;i++)
-	{
-		printf("%d ",arr[i]);
-	}
-	printf("\n");
+     int i;
+     
+     if(isEmpty())
+     {
+         printf("\nQueue is empty.\n\n");
+         return;
+     }
+     
+     printf("\nQueue: ");
+
+     for(i=0;i<=rear;i++)
+     {
+          printf("%d ",Arr[i]);
+     }
+     printf("\n\n");
 }
 
-void pop(int num)
+void Front()
 {
-	int i;
-	for(i=0;i<n;i++)
-	{
-		//printf("Deleted element %d: %d\n",i+1,arr[i]);	
-		arr[i] = arr[i+1];
-	}
-	top--;
-}
-
-void  Front()
-{
-	if(isEmpty())
-	{
-		printf("Queue is empty.");
-		return;
-	}
-	printf("The element at front: %d",arr[0]);
+     if(isEmpty())
+     {
+         printf("\nQueue is empty.\n\n");
+         return;
+     }
+    
+     printf("\nElement at front: %d\n\n",Arr[0]);
 }
